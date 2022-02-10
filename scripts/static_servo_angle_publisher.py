95858e4919faa116f79e613b8f924b2331a8349f
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2022-02-10 16:11:56 +0000

    Compute forward kinematics and print poses

--- scripts/static_servo_angle_publisher.py
+++ scripts/static_servo_angle_publisher.py
@@ -4,15 +4,42 @@ from std_msgs.msg import Float64
 import numpy as np
 
 
+def tf_from_dh(
+    alpha: float, a: float, d: float, theta: float
+) -> np.ndarray:  # type: ignore
+    """
+    Converts DH parameters to a 3D homogenous transformation matrix.
+    """
+
+    return np.array(  # type: ignore
+        [
+            [np.cos(theta), -np.sin(theta), 0, a],
+            [
+                np.sin(theta) * np.cos(alpha),
+                np.cos(theta) * np.cos(alpha),
+                -np.sin(alpha),
+                -np.sin(alpha) * d,
+            ],
+            [
+                np.sin(theta) * np.sin(alpha),
+                np.cos(theta) * np.sin(alpha),
+                np.cos(alpha),
+                np.cos(alpha) * d,
+            ],
+            [0, 0, 0, 1],
+        ]
+    )
+
+
 def main():
     # Change these (send to robot)
     # All angles are in radians
-    joint1_angle = -0.5 * np.pi
-    joint2_angle = 0.5
-    joint3_angle = -0.5 * np.pi
-    joint4_angle = 0.5 * np.pi
+    joint1_angle = -np.pi / 2.0
+    joint2_angle = 0
+    joint3_angle = 0
+    joint4_angle = 0
 
-    gripper_angle = 1.0
+    gripper_angle = 0.05
     gripper_sub_angle = 0.0
 
     rospy.init_node("static_servo_angle_publisher")
@@ -44,11 +71,31 @@ def main():
     )
 
     while not rospy.is_shutdown():
+        # Compute forward kinematics
+        T01 = tf_from_dh(0, 0, 0, joint1_angle - np.pi / 2.0)
+        T12 = tf_from_dh(0.0, 0, 0.077, 0)
+        T23 = tf_from_dh(np.pi / 2.0, 0.0, 0.0, joint2_angle + np.pi / 2.0 + 0.1853)
+        T34 = tf_from_dh(0, 0.13, 0, joint3_angle + np.pi / 2.0 - 0.1853)
+        T45 = tf_from_dh(0, 0.124, 0, joint4_angle)
+        T5G = tf_from_dh(0, 0.126, 0, 0)
+        tool_pose = T01 @ T12 @ T23 @ T34 @ T45 @ T5G @ np.array([0, 0, 0, 1])
+
+        # Print joint and gripper (tool) poses
+        print("-------------------------------")
+        print("Joint 1 pose:", T01 @ np.array([0, 0, 0, 1]))
+        print("Joint 2 pose:", T01 @ T12 @ T23 @ np.array([0, 0, 0, 1]))
+        print("Joint 3 pose:", T01 @ T12 @ T23 @ T34 @ np.array([0, 0, 0, 1]))
+        print("Joint 4 pose:", T01 @ T12 @ T23 @ T34 @ T45 @ np.array([0, 0, 0, 1]))
+        print("Tool pose:", tool_pose[:3])
+        print("-------------------------------")
+
+        # Publish joint angles to Gazebo topics
         joint1_publisher.publish(joint1_angle)
         joint2_publisher.publish(joint2_angle)
         joint3_publisher.publish(joint3_angle)
         joint4_publisher.publish(joint4_angle)
 
+        # Publish gripper opening angles to Gazebo topics
         gripper_publisher.publish(gripper_angle)
         gripper_sub_publisher.publish(gripper_sub_angle)
 
