e2fc8a09e70008ea36470c3f431a24859e066a8f
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2022-02-14 11:35:59 +0000

    Clean up static servo angle publisher

--- scripts/static_servo_angle_publisher.py
+++ scripts/static_servo_angle_publisher.py
@@ -4,7 +4,8 @@ import rospy
 from std_msgs.msg import Float64
 import numpy as np
 
-from open_manipulator_tenaci.dh import tf_from_dh
+from open_manipulator_tenaci.forward_kinematics import forward_kinematics
+from open_manipulator_tenaci.kinematics_common import JointAngles
 
 
 def main():
@@ -47,23 +48,10 @@ def main():
     )
 
     while not rospy.is_shutdown():
-        # Compute forward kinematics
-        T01 = tf_from_dh(0, 0, 0, joint1_angle + np.pi / 2.0)
-        T12 = tf_from_dh(0.0, 0, 0.077, 0)
-        T23 = tf_from_dh(np.pi / 2.0, 0.0, 0.0, -joint2_angle + np.pi / 2.0 - 0.1853)
-        T34 = tf_from_dh(0, 0.13, 0, -joint3_angle - np.pi / 2.0 + 0.1853)
-        T45 = tf_from_dh(0, 0.124, 0, -joint4_angle)
-        T5G = tf_from_dh(0, 0.126, 0, 0)
-        tool_pose = T01 @ T12 @ T23 @ T34 @ T45 @ T5G @ np.array([0, 0, 0, 1])
-
-        # Print joint and gripper (tool) poses
-        print("-------------------------------")
-        print("Joint 1 pose:", T01 @ np.array([0, 0, 0, 1]))
-        print("Joint 2 pose:", T01 @ T12 @ T23 @ np.array([0, 0, 0, 1]))
-        print("Joint 3 pose:", T01 @ T12 @ T23 @ T34 @ np.array([0, 0, 0, 1]))
-        print("Joint 4 pose:", T01 @ T12 @ T23 @ T34 @ T45 @ np.array([0, 0, 0, 1]))
-        print("Tool pose:", tool_pose[:3])
-        print("-------------------------------")
+        # Print joint and tool pose
+        forward_kinematics(
+            JointAngles(joint1_angle, joint2_angle, joint3_angle, joint4_angle), True
+        )
 
         # Publish joint angles to Gazebo topics
         joint1_publisher.publish(joint1_angle)
